b8e3e86e202cecebb51559a085ada58e0b893c5c
Tex <james.davis@zenlist.com>
2025-04-20 16:52:05 -0500

    whof ucking knows?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disable automatic static optimization
+  experimental: {
+    // In Next.js 15, you might need to use these options
+    // to further control SSR behavior
+    ppr: false,
+    serverActions: false,
+  },
 };
 
 export default nextConfig;
