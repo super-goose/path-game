430bd6d4175c5720ecd91e425e52026623243aaf
Tex <james.davis@zenlist.com>
2025-05-01 10:10:32 -0500

    move game into new component

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,1 +1,19 @@
-export const Game = () => {};
+"use client";
+
+import { Inter } from "next/font/google";
+import { Header } from "@/components/header";
+import { Board } from "@/components/board";
+import { NextUp } from "@/components/next-tile";
+import styles from "@/styles/home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const Game = () => {
+  return (
+    <main className={`${styles.main} ${inter.className}`}>
+      <Header />
+      <Board />
+      <NextUp />
+    </main>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 "use client";
 
 import Head from "next/head";
-import { Inter } from "next/font/google";
-import styles from "@/styles/home.module.css";
-import { Header } from "@/components/header";
-import { Board } from "@/components/board";
-import { NextUp } from "@/components/next-tile";
 import { Provider } from "react-redux";
 import store from "@/state/store";
 import NoSSR from "@/components/no-ssr/no-ssr";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Game } from "@/components/game";
 
 export default function Home() {
   return (
@@ -22,11 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NoSSR>
-        <main className={`${styles.main} ${inter.className}`}>
-          <Header />
-          <Board />
-          <NextUp />
-        </main>
+        <Game />
       </NoSSR>
     </Provider>
   );
