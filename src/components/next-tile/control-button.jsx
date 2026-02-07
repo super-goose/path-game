b85afb769142ab97cac63c98dd96075f7968fc94
Tex <james.davis@zenlist.com>
2026-02-07 12:04:49 -0600

    rotate button shadow endcap

--- src/components/next-tile/control-button.jsx
+++ src/components/next-tile/control-button.jsx
@@ -3,6 +3,7 @@ import style from "./control-button.module.css";
 import { ControlButtonButton } from "./wrappers";
 
 const ARC_PATH = "M 65,35 A 25,25 0,1,1 35,35";
+const ARC_PATH_SHADOW_ENDCAP = "M 57.5,42.5 L 70.3,25.7";
 const ROTATE_ARROW_PATH = "M 15,25 L 55,15 L 45,55 Z";
 const RotateArrow = ({ direction }) => (
   <svg
@@ -16,6 +17,13 @@ const RotateArrow = ({ direction }) => (
     viewBox="0, 0, 100, 100"
   >
     <g>
+      <path
+        d={ARC_PATH_SHADOW_ENDCAP}
+        fill="none"
+        strokeWidth={3}
+        stroke="var(--accent-color)"
+        strokeOpacity={1}
+      />
       <path
         d={ARC_PATH}
         fill="none"
