85ec066a09a91e8597148c7736179f9a09c5cf97
Tex <james.davis@zenlist.com>
2026-02-07 11:52:19 -0600

    better rotate arrow

--- src/components/next-tile/control-button.jsx
+++ src/components/next-tile/control-button.jsx
@@ -3,8 +3,7 @@ import style from "./control-button.module.css";
 import { ControlButtonButton } from "./wrappers";
 
 const ARC_PATH = "M 65,35 A 25,25 0,1,1 35,35";
-const ROTATE_ARROW_PATH = "M 33,35 L 36,34 L 35,37 Z";
-const ROTATE_ARROW_PATH_SHADOW = "M 30,35 L 36,34 L 35,40 Z";
+const ROTATE_ARROW_PATH = "M 15,25 L 55,15 L 45,55 Z";
 const RotateArrow = ({ direction }) => (
   <svg
     style={
@@ -18,7 +17,13 @@ const RotateArrow = ({ direction }) => (
   >
     <g>
       <path
-        id={"idShadow"}
+        d={ARC_PATH}
+        fill="none"
+        strokeWidth={21}
+        stroke="var(--accent-color)"
+        strokeOpacity={1}
+      />
+      <path
         d={ARC_PATH}
         fill="none"
         strokeWidth={15}
@@ -26,48 +31,12 @@ const RotateArrow = ({ direction }) => (
         strokeOpacity={1}
       />
       <path
-        id={"idShadow3"}
-        d={ROTATE_ARROW_PATH_SHADOW}
-        fill="none"
-        strokeWidth={20}
-        stroke="var(--light-background)"
+        d={ROTATE_ARROW_PATH}
+        fill="var(--light-background)"
+        strokeWidth={3}
+        stroke="var(--accent-color)"
         strokeOpacity={1}
         transform="rotate(-15deg)"
-      />
-    </g>
-  </svg>
-);
-
-const LINE_PATH = "M 50,90 L 50,25";
-const LINE_ARROW_PATH = "M 50,10 L 75,40 L 25,40 Z";
-
-const SwitchArrow = ({ direction }) => (
-  <svg
-    style={
-      direction === "DOWN"
-        ? {
-            transform: "scaleY(-1)",
-          }
-        : {}
-    }
-    viewBox="0, 0, 100, 100"
-  >
-    <g>
-      <path
-        id={"idShadow"}
-        d={LINE_PATH}
-        fill="none"
-        strokeWidth={15}
-        stroke="var(--light-background)"
-        strokeOpacity={1}
-      />
-      <path
-        id={"idShadow3"}
-        d={LINE_ARROW_PATH}
-        fill="var(--light-background)"
-        strokeWidth={1}
-        stroke="var(--light-background)"
-        strokeOpacity={1}
       />
     </g>
   </svg>
@@ -76,8 +45,6 @@ const SwitchArrow = ({ direction }) => (
 const SYMBOL = {
   cw: <RotateArrow direction="CW" />,
   ccw: <RotateArrow direction="CCW" />,
-  prev: <SwitchArrow direction="UP" />,
-  next: <SwitchArrow direction="DOWN" />,
 };
 
 export const ControlButton = ({ direction, onClick }) => (
